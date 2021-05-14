31f1498b1f4688cee98d846cbe61323ef910be91
Musab-Al-omari <musabsomari@gmail.com>
2021-05-14 08:19:05 +0300

    sad

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
-arr = [1, 2, 3, 4, 5]
-arr2 = [1, 2, 3, 4, 5, 6]
-const notInFirstArray = (arr, arr2) => {
-  // Solution code here...
-  return arr2.filter(value => {
-    return !arr.includes(value)
-  })
-};
-console.log(notInFirstArray(arr, arr2));
+let x = [1, 2, 3]
+let y = [35, 8, 54]
+x.push(y)
+console.log(x);
